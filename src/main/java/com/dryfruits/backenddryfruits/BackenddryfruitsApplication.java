368cc38bff985c048abbd511e9527395578bc3e2
swaadik262-prog <swaadik262@gmail.com>
2025-11-19 17:13:48 +0530

    Add files via upload
    add files

---
+++ src/main/java/com/dryfruits/backenddryfruits/BackenddryfruitsApplication.java
@@ -0,0 +1,13 @@
+package com.dryfruits.backenddryfruits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackenddryfruitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackenddryfruitsApplication.class, args);
+	}
+
+}
